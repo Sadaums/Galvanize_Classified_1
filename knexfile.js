ef0443daf301d594960754b3163a6ac7d9f55578
Kevin Conrad <KevinDangerConrad@gmail.com>
2017-05-17 17:25:16 -0600

    have seeds + migrations done

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,16 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
-
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/assessment2'
+    },
+    test: {
+      client: 'pg',
+      connection: 'postgres://localhost/assessment2'
+    },
+    production: {
+        client: 'pg',
+        connection: 'postgres://localhost/assessment2'
+    },
 };
